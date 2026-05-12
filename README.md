8273542a419336bcb7061c2c6ff847e09971f7b6
HideInGermany <antonihess5@gmail.com>
2026-05-12 18:09:36 +0200

    Initial commit - projekt setup + pierwsze okno SFML

---
+++ README.md
@@ -0,0 +1,15 @@
+# BattleArena 🎮
+
+Gra 2D z widokiem z góry dla dwóch graczy. Projekt zaliczeniowy z programowania obiektowego w C++.
+
+## Technologie
+- C++17
+- SFML 2.6.1
+
+## Jak uruchomić
+1. Sklonuj repozytorium
+2. Zbuduj SFML ze źródeł (GCC 13.3.0)
+3. Skompiluj projekt: `g++ -std=c++17 src/main.cpp ...`
+
+## Autorzy
+- (twoje imię)
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#define SFML_STATIC
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "BattleArena");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
